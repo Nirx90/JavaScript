2e474420f173324d665b941538867bea0fe7849a
Nirx90 <123075342+Nirx90@users.noreply.github.com>
2024-04-13 20:16:51 +0530

    Update first.js

--- first.js
+++ first.js
@@ -937,3 +937,23 @@ let ar1 = [5,10,8,130,44]
 let isLarge = (elem) => elem>9;
 
 console.log(ar1.findIndex(isLarge))
+
+// -------------------------------10-04-2024--------------------------------------
+
+let ar1 = [5,10,8,130,44]
+
+// console.log(ar1.findIndex((elem,idx) => elem>8 ))
+
+// console.log(ar1.findIndex((elem,idx) => {console.log(elem)} ))
+
+// console.log(
+//     ar1.findIndex((em,id) =>{
+//         console.log(id)
+//     })
+// )
+
+// console.log(
+//     ar1.findIndex((em,id) =>{
+//         return em<18
+//     })
+// )
